cacbf1daec7d7d58abec6e7c368f3e8eda0c300f
mathiaszanotto <mathiassz@al.insper.edu.br>
2025-08-25 15:37:19 -0300

    exe5.1

--- exe5/main.c
+++ exe5/main.c
@@ -3,9 +3,35 @@
 #include "hardware/gpio.h"
 #include "pico/stdlib.h"
 
+#define BTN_PIN 28
+
+volatile int flag = 0;
+
+void btn_callback(uint gpio, uint32_t events) {
+    if (events==0x04) flag = 1;
+    else if (events==0x08) flag = 0;
+}
+
 int main() {
     stdio_init_all();
 
+    gpio_init(BTN_PIN);
+    gpio_set_dir(BTN_PIN, GPIO_IN);
+    gpio_pull_up(BTN_PIN);
+
     while (true) {
+        gpio_set_irq_enabled_with_callback(
+        BTN_PIN, GPIO_IRQ_EDGE_RISE | GPIO_IRQ_EDGE_FALL, true, &btn_callback);
+        if (flag) {
+            sleep_ms(800);
+            gpio_set_irq_enabled_with_callback(
+            BTN_PIN, GPIO_IRQ_EDGE_RISE | GPIO_IRQ_EDGE_FALL, true, &btn_callback);
+            if (flag) {
+                printf("Aperto longo!\n");
+
+            } else {
+                printf("Aperto curto!\n");
+            }
+        }
     }
 }
