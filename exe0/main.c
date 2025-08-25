2bcfa438bd217c2de8c97b0de57aae9db357c281
mathiaszanotto <mathiassz@al.insper.edu.br>
2025-08-25 10:52:11 -0300

    exe0.5

--- exe0/main.c
+++ exe0/main.c
@@ -1,48 +1,42 @@
 #include "hardware/gpio.h"
 #include "pico/stdlib.h"
 #include <stdio.h>
+#include <stdint.h>
 
 #define BTN_PIN_R 28
 
 volatile int btn_flag = 0;
 
-void btn_callback(uint gpio, uint32_t events) {
-  if (events == 0x4) { // fall edge
-
-    printf("btn pressed \n");
-
-    while (!gpio_get(BTN_PIN_R)) {
-      sleep_ms(1);
-    }
-
-    printf("btn released \n");
-
-    sleep_ms(1);
-    btn_flag = 1;
-  }
+static void btn_callback(uint gpio, uint32_t events) {
+    if (gpio == BTN_PIN_R && (events & GPIO_IRQ_EDGE_FALL)) btn_flag = 1;
 }
 
 int main(void) {
-  stdio_init_all();
+    stdio_init_all();
+    gpio_init(BTN_PIN_R);
+    gpio_set_dir(BTN_PIN_R, GPIO_IN);
+    gpio_pull_up(BTN_PIN_R);
 
-  gpio_init(BTN_PIN_R);
-  gpio_set_dir(BTN_PIN_R, GPIO_IN);
-  gpio_pull_up(BTN_PIN_R);
+    gpio_set_irq_enabled_with_callback(BTN_PIN_R,GPIO_IRQ_EDGE_FALL,true,&btn_callback);
 
-  gpio_set_irq_enabled_with_callback(BTN_PIN_R, GPIO_IRQ_EDGE_FALL, true,
-                                     &btn_callback);
-  
-  int capture_flag = 0;
+    int capture_flag = 0;
 
-  while (1) {
-    if (btn_flag) {
-      capture_flag = 1;
-      btn_flag = 0;
+    while (true) {
+        if (btn_flag) {
+            btn_flag = 0;
+            printf("btn pressed \n");
+
+            while (!gpio_get(BTN_PIN_R)) {
+                sleep_ms(1);
+            }
+            sleep_ms(1);
+            printf("btn released \n");
+
+            capture_flag = 1;
+        }
+
+        if (capture_flag) capture_flag = 0;
+
+        tight_loop_contents();
     }
-
-    if (capture_flag) {
-      capture_flag = 0;
-    }
-
-  }
 }
